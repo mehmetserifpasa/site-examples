933cea97da3aca05666df6e20d5e1fb1ed1f660a
mehmetserifpasa <=>
2021-09-21 19:17:05 +0300

    .

---
+++ 3/js/main.js
@@ -0,0 +1,8 @@
+
+function kapat() {
+    document.getElementById('contact').style.visibility = "hidden";
+}
+
+function ac() {
+    document.getElementById('contact').style.visibility = "unset";
+}
